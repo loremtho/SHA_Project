d51b1bedd71116c8289c4c7919f473302be4bcf1
loremtho <skythomas33@gmail.com>
2024-08-19 17:19:58 +0900

    html 연결 완

--- mysite/imagegen/connectStable.py
+++ mysite/imagegen/connectStable.py
@@ -1,30 +1,36 @@
 # from diffusers import DiffusionPipeline
-from diffusers import FluxPipeline
+# from diffusers import FluxPipeline
+from diffusers import StableDiffusion3Pipeline
+from diffusers import StableDiffusionPipeline
+from diffusers import DiffusionPipeline
 from django.conf import settings
 import torch
 from datetime import datetime
 import os
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "true"
 
-def createIMG(prompt_text):
+def createIMG(prompt_text, model_id, num_inference_steps, guidance_scale):
     # 모델 로드
     
     from huggingface_hub import login
     huggingface_token = "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"
     login(token=huggingface_token)
     
-    model_id = "black-forest-labs/FLUX.1-dev"
-    pipe = FluxPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-    pipe.load_lora_weights("XLabs-AI/flux-RealismLora")
+    model_id = "Niggendar/duchaitenPonyXLNo_v35"
+    # pipe = StableDiffusion3Pipeline.from_pretrained(model_id, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+    pipe = DiffusionPipeline.from_pretrained(model_id)
+    # pipe.load_lora_weights("XLabs-AI/flux-RealismLora")
     pipe = pipe.to("cuda")  # GPU 사용
+    
+    torch.cuda.empty_cache()
     
     pipe.enable_xformers_memory_efficient_attention()
 
 
     # 세부 설정
     prompt = prompt_text
-    num_inference_steps = 100  # 생성 단계 수
-    guidance_scale = 7  # CFG 스케일
+    # num_inference_steps = 1  # 생성 단계 수
+    # guidance_scale = 0  # CFG 스케일
 
 
     # 이미지 생성
--- mysite/imagegen/views.py
+++ mysite/imagegen/views.py
@@ -9,11 +9,16 @@ def image_generate_view(request):
     if request.method == "POST":
         prompt = request.POST.get("prompt")
         negative_prompt = request.POST.get("negative_prompt", "")
+        model_id = request.POST.get('model_id')
+        num_inference_steps = int(request.POST.get('num_inference_steps'))
+        guidance_scale = float(request.POST.get('guidance_scale'))
 
         # 한글 설명을 영어로 번역
-        english_text = translate_to_english(prompt)
+        # english_text = translate_to_english(prompt)
+        english_text = prompt
         if negative_prompt:
-            negative_english_text = translate_to_english(negative_prompt)
+            # negative_english_text = translate_to_english(negative_prompt)
+            negative_english_text = negative_prompt
         else:
             negative_english_text = None
 
@@ -21,11 +26,15 @@ def image_generate_view(request):
         prompt_text= transform_to_stable_diffusion_prompt(english_text, negative_english_text)
 
         # 이미지 생성
-        createIMG(prompt_text)
+        # createIMG(prompt_text)
+        
+        image_url = createIMG(prompt_text, model_id, num_inference_steps, guidance_scale)
 
         # 이미지 경로 설정
         image_folder = 'media/picture/'
         image_filename = sorted(os.listdir(image_folder))[-1]  # 가장 최근 생성된 이미지 선택
         image_url = os.path.join(image_folder, image_filename)
+        
+        return render(request, 'imagegen/generate.html', {'image_url': image_url})
 
-    return render(request, 'imagegen/generate.html', {"image_url": image_url})
+    return render(request, 'imagegen/generate.html')
